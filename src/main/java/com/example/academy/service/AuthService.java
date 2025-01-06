84d180dcdd70ec5a638a0fef784a979c2a5c7cc4
orukdoline <skynets@kakao.com>
2025-01-06 16:30:06 +0900

    진수우) 디버그기능 활성화.

--- src/main/java/com/example/academy/service/AuthService.java
+++ src/main/java/com/example/academy/service/AuthService.java
@@ -8,8 +8,11 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.academy.dto.PasswordModifyDTO;
 import com.example.academy.mapper.AuthMapper;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
 @Transactional
+@Slf4j
 public class AuthService {
 	@Autowired private BCryptPasswordEncoder bCryptPasswordEncoder;
 	@Autowired AuthMapper authMapper;
@@ -21,6 +24,7 @@ public class AuthService {
 		// 데이터베이스에서 기존 비밀번호가 일치하는지 확인.
 		Integer nowPassword = authMapper.selectEmployeeNowPw(passwordModifyDTO);
 		// 입력한 비밀번호와 데이터베이스에 있는 비밀번호가 일치하다면,
+		log.debug("result ---------------------------------------------------------------" + nowPassword);
 		if (nowPassword == 1) {
 			// 새 비밀번호와 새 비밀번호 확인에 입력한 값이 일치하다면,
 			if(passwordModifyDTO.getEmployeeChangePw().equals(passwordModifyDTO.getEmployeeChangePwCheck())) {
@@ -30,8 +34,8 @@ public class AuthService {
 				authMapper.updateEmployeePw(passwordModifyDTO);
 				return 1; // 비밀번호 수정 성공.
 			}
-			return 2; // 새 비밀번호와 새 비밀번호 확인에 입력한 값이 일치하지 않음.
+			else return 2; // 새 비밀번호와 새 비밀번호 확인에 입력한 값이 일치하지 않음.
 		}
-		return 3; // 기존 비밀번호가 일치하지 않음.
+		else return 3; // 기존 비밀번호가 일치하지 않음.
 	}
 }
