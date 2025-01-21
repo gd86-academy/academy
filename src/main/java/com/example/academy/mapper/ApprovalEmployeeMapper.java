645a692ea619c0ec6f81218e57a2eecea5aee5f5
hyerin716 <hyerin71699@gmail.com>
2025-01-21 12:03:12 +0900

    김혜린) 근태신청 결재 구현완료

--- src/main/java/com/example/academy/mapper/ApprovalEmployeeMapper.java
+++ src/main/java/com/example/academy/mapper/ApprovalEmployeeMapper.java
@@ -11,6 +11,15 @@ import com.example.academy.dto.AttendanceApprovalOneDTO;
 @Mapper
 public interface ApprovalEmployeeMapper {
 	
+	// 김혜린 : 근태신청서 총 결재자 수 구하기
+	Integer selectTotalApprover(AttendanceApprovalOneDTO attendanceApprovalOneDTO);
+	
+	// 김혜린 : 근태신청서 결재 승인시 - 결재자 상태 업데이트
+	Integer updateApprovalStatusAgree(AttendanceApprovalOneDTO attendanceApprovalOneDTO);
+	
+	// 김혜린 : 근태신청서 결재 반려시 - 결재자 상태 업데이트
+	Integer updateApprovalStatusReject(AttendanceApprovalOneDTO attendanceApprovalOneDTO);
+	
 	// 김혜린 : 근태신청서 상세페이지 - 결재자 목록
 	List<AttendanceApprovalOneDTO> approverList(Integer attendanceApprovalNo);
 	
