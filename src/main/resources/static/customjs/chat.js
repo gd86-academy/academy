54f3d1ee38b0cb91836ed4ea958c639702ec5018
DESKTOP-4SJR2LC\hsw52 <hsw52233@gmail.com>
2025-01-23 12:38:59 +0900

    하상우) chat.html 수정

--- src/main/resources/static/customjs/chat.js
+++ src/main/resources/static/customjs/chat.js
@@ -59,19 +59,103 @@ document.addEventListener('alpine:init', () => {
 					        ],
 					    }));
 				
-				                
+						// 테이블 출력
+							Alpine.data('multicolumn', () => ({
+							    datatable: null,
+							    init() {
+							        // AJAX 요청
+							        $.ajax({
+							            url: 'http://localhost/academy/restapi/employeeList',
+							            type: 'GET',
+							            dataType: 'json',
+							            success: (data) => {
+											this.datatable = new simpleDatatables.DataTable('#myTable', {
+											    data: {
+											       
+											        data: data.map(item => [
+														[item[0], item[5]],	// 이미지파일 + 사원
+											        ])
+											    },
+											    searchable: true,
+											    perPage: 5,
+											    perPageSelect: [10, 20, 30, 50, 100],
+												
+											    columns: [
+											        {
+											            select: 0,
+											            render: (data, cell, row) => {
+															const [name, image] = data.split(','); 
+															if(image == 'null.null') {
+																return `
+																	<div class="flex items-center w-max">
+																		<img class="w-9 h-9 rounded-full ltr:mr-2 rtl:ml-2 object-cover" src="./images/defaultProfile.png" />
+																		<span>${name}</span>
+																	</div>
+																`;
+															} else {
+																return `
+																	<div class="flex items-center w-max">
+																		<img class="w-9 h-9 rounded-full ltr:mr-2 rtl:ml-2 object-cover" src="./upload/${image}" />
+																		&nbsp;&nbsp;&nbsp;<span>${name}</span>
+																	</div>
+																`;
+															}
+											            },
+														sortable: true,
+											        },
+													
+											        
+											    ],
+											
+							                    firstLast: true,
+							                    firstText:
+							                        '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M13 19L7 12L13 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path opacity="0.5" d="M16.9998 19L10.9998 12L16.9998 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+							                    lastText:
+							                        '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M11 19L17 12L11 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path opacity="0.5" d="M6.99976 19L12.9998 12L6.99976 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+							                    prevText:
+							                        '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M15 5L9 12L15 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+							                    nextText:
+							                        '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M9 5L15 12L9 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+							                    
+							                    layout: {
+							                        top: '{search}',
+							        
+							                    },
+												
+							                });
+											
+											// 행 클릭 이벤트
+											document.querySelector('#myTable tbody').addEventListener('click', (e) => {
+											    const rowElement = e.target.closest('tr'); // 클릭된 행의 인덱스.
+											    if (rowElement) {
+													const tdElements = rowElement.querySelectorAll('td');
+													const employeeNo = tdElements[1].textContent; // 두번째 열 데이터 추출.
+													window.location.href = `/academy/employeeOne?employeeNo=${employeeNo}`;
+											    }
+											});
+							            },
+							            error: (xhr, status, error) => {
+							                console.error('Error:', error);
+							            }
+							        });
+							    },
+
+						  // Method to handle user selection
+						  selectUser(person) {
+						    this.selectedUser = person;
+						    // You can handle any logic when a user is selected, such as opening a chat or displaying user details
+						    console.log('Selected User:', person);
+						  }
+						}));
+
+
                 //chat
                 Alpine.data('chat', () => ({
                     isShowUserChat: false,
                     isShowChatMenu: false,
-                    loginUser: {
-                        id: 0,
-                        name: 'Alon Smith',
-                        path: 'profile-34.jpeg',
-                        designation: 'Software Developer',
-                    },
+                    
                     contactList: [
-                        {
+                        /*{
                             userId: 1,
                             name: 'Nia Hillyer',
                             path: 'profile-16.jpeg',
@@ -105,7 +189,7 @@ document.addEventListener('alpine:init', () => {
                                 },
                             ],
                             active: true,
-                        },
+                        },*/
                         {
                             userId: 2,
                             name: 'Sean Freeman',
