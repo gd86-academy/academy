9e3fe8cd8ec2d78cdafbd9c0bd8dc58ea891d181
seyoung512 <seyoung512@naver.com>
2025-02-06 16:12:26 +0900

    세영) 휴일 service 삭제

--- src/main/java/com/example/academy/service/ScheduledService.java
+++ src/main/java/com/example/academy/service/ScheduledService.java
@@ -13,7 +13,6 @@ import com.example.academy.dto.AttendanceDTO;
 import com.example.academy.dto.EmployeeListDTO;
 import com.example.academy.mapper.EmployeeMapper;
 import com.example.academy.mapper.ScheduledMapper;
-import com.example.academy.vo.Holiday;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,33 +23,15 @@ public class ScheduledService {
 
 	@Autowired private ScheduledMapper scheduledMapper;
 	@Autowired private EmployeeMapper employeeMapper;
-	@Autowired private HolidayService holidayService;
 	
 	// 00시마다 모든 사원 근태 데이터 생성
-	@Scheduled(cron = "0 26 09 * * ?")
+	@Scheduled(cron = "0 00 00 * * ?")
 	public void generateAttendanceData() {
 		// 현재 시간정보를 가져옴.
 		LocalDate today = LocalDate.now();
-		String year = Integer.toString(today.getYear());
-		
-		// 공휴일 확인
-		List<Holiday> holidays = holidayService.getHolidayList(year);
-		
-		boolean isHoliday = holidays.stream()
-                					.anyMatch(holiday -> holiday.getDate().equals(today.toString())); // 공휴일 여부 체크
-		log.debug("----------------------->" + holidays);
-		log.debug("----------------------->" + isHoliday);
-		
-		if (isHoliday) {
-			log.debug("오늘은 공휴일입니다. 근태 데이터 생성을 생략합니다.");
-			return;  // 공휴일인 경우 데이터 생성을 중지
-		} else {
-			log.debug("오늘은 공휴일이 아닙니다.");
-		}
 		
 		// 현재 시간정보가 주말이 아니라면,
 		if (today.getDayOfWeek() != DayOfWeek.SATURDAY && today.getDayOfWeek() != DayOfWeek.SUNDAY) {
-			
 			
 			log.debug("근태 데이터를 생성합니다.");
 	        
@@ -60,6 +41,7 @@ public class ScheduledService {
 	        // 2. 근태 데이터 생성
 	        for(EmployeeListDTO employeeListDTO : employees) {
 	        	scheduledMapper.insertAttendance(employeeListDTO.getEmployeeNo());
+	        	log.debug("--------------------사원 " + employeeListDTO.getEmployeeNo() + "의 근태 데이터 생성");
 	        	String attendanceApprovalType = scheduledMapper.selectContent(employeeListDTO.getEmployeeNo());
 	        	
 	        	// DTO에 employeeNo,content 넣기
@@ -70,10 +52,12 @@ public class ScheduledService {
 	        	// 3. 근태 신청서가 승인된 경우, 근태유형에 따라 근태 정보 수정
 	            if (attendanceApprovalType != null && (attendanceApprovalType.equals("CT003") || attendanceApprovalType.equals("CT004"))) { // 근태유형이 오전반차, 오후반차일 시 근태유형만 변경
 	                scheduledMapper.updateAttendance(attendance);
+	                log.debug("----------------------사원 " + employeeListDTO.getEmployeeNo() + "의 근태 데이터 변경(반차)");
 	            } else if(attendanceApprovalType != null && !attendanceApprovalType.isEmpty()) { // 근태유형이 NULL이 아니거나 값이 있으면 근태유형, 출근, 퇴근 시간 변경 
-	            	scheduledMapper.updateAttendanceByCheckTime(attendance);         
+	            	scheduledMapper.updateAttendanceByCheckTime(attendance);
+	            	log.debug("----------------------사원 " + employeeListDTO.getEmployeeNo() + "의 근태 데이터 변경");
 	            } else {	
-	            	log.debug("사원 " + employeeListDTO.getEmployeeNo() + "의 승인된 근태 신청서가 없습니다.");
+	            	log.debug("----------------------사원 " + employeeListDTO.getEmployeeNo() + "의 승인된 근태 신청서가 없습니다.");
 	            }
 	        }
 		}
